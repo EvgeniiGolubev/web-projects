acf74e075f10e571cc0986bd21ab91cef89171f6
Evgenii Golubev <golubevevgenii95@yandex.ru>
2022-08-02 02:27:58 +0300

    stupid commit

--- sweater/src/main/java/com/example/sweater/service/UserService.java
+++ sweater/src/main/java/com/example/sweater/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,6 +26,9 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Value("${hostname}")
+    private String hostname;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -58,8 +62,8 @@ public class UserService implements UserDetailsService {
 
     private void sendMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
-            String message = String.format("Hello, %s! \nWelcome to Sweater. Please, visit next link http://localhost:8080/activate/%s",
-                     user.getUsername(), user.getActivationCode());
+            String message = String.format("Hello, %s! \nWelcome to Sweater. Please, visit next link http://$s/activate/%s",
+                     user.getUsername(), hostname, user.getActivationCode());
 
             mailSenderService.send(user.getEmail(), "Activation code", message);
         }
